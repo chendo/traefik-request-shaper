d66ef92c99e33d89a3d06b01046dd5d3cff15e42
chendo <chendo@users.noreply.github.com>
2022-10-08 19:07:39 +1100

    Improve config by allowing time durations, add a README

--- request_shaper.go
+++ request_shaper.go
@@ -24,16 +24,21 @@ type Exclusion struct {
 
 type Config struct {
 	Average         int64                    `yaml:"average"`
-	Period          ptypes.Duration          `yaml:"period"`
-	MaxDelay				ptypes.Duration					 `yaml:"max_delay"`
+	Period          string          `yaml:"period"`
+	MaxDelay				string					 `yaml:"maxDelay"`
+	ExceedWait      string					 `yaml:"exceedWait"` 
 	Burst           int64                    `yaml:"burst"`
+	TTL   					string					 `yaml:"ttl"` // how long before we expire entries
 	Exclusion       *Exclusion               `yaml:"exclusion"`
 }
 
 func CreateConfig() *Config {
 	return &Config{
 		Average: 0,
-		Period: 0,
+		MaxDelay: "5s",
+		ExceedWait: "0s",
+		Period: "1s",
+		TTL: "60s",
 		Burst: 0,
 	}
 }
@@ -44,9 +49,9 @@ type requestShaper struct {
 	name  string
 	rate  rate.Limit // reqs/s
 	burst int64
-	// maxDelay is the maximum duration we're willing to wait for a bucket reservation to become effective, in nanoseconds.
-	// For now it is somewhat arbitrarily set to 1/(2*rate).
-	maxDelay time.Duration
+	
+	maxDelay time.Duration // maxDelay is the maximum duration we're willing let the user to wait before we return an error
+	exceedWait time.Duration // how long we make the client wait when they exceed maxDelay
 	// each rate limiter for a given source is stored in the buckets ttlmap.
 	// To keep this ttlmap constrained in size,
 	// each ratelimiter is "garbage collected" when it is considered expired.
@@ -57,11 +62,8 @@ type requestShaper struct {
 	buckets *TtlMap // actual buckets, keyed by source.
 }
 
-// New returns a rate limiter middleware.
+// New returns a request shaper middleware.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-
-
-
 	buckets, err := NewConcurrent(maxSources)
 	if err != nil {
 		return nil, err
@@ -72,7 +74,10 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 		burst = 1
 	}
 
-	period := time.Duration(config.Period)
+	period, err := time.ParseDuration(config.Period)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse period: %v, err: %s", period, err)
+	}
 	if period < 0 {
 		return nil, fmt.Errorf("negative value not valid for period: %v", period)
 	}
@@ -80,54 +85,68 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 		period = time.Second
 	}
 
+	ttl, err := time.ParseDuration(config.TTL)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse ttl: %v", period)
+	}
+	if ttl < 0 {
+		return nil, fmt.Errorf("negative value not valid for ttl: %v", period)
+	}
+	if ttl == 0 {
+		// ttl of zero would effectively be useless
+		ttl = time.Second
+	}
+
+	exceedWait, err := time.ParseDuration(config.ExceedWait)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse exceedWait: %v", period)
+	}
 	// if config.Average == 0, in that case,
 	// the value of maxDelay does not matter since the reservation will (buggily) give us a delay of 0 anyway.
 	var maxDelay time.Duration
 	var rtl float64
 	if config.Average > 0 {
 		rtl = float64(config.Average*int64(time.Second)) / float64(period)
-		// maxDelay does not scale well for rates below 1,
-		// so we just cap it to the corresponding value, i.e. 0.5s, in order to keep the effective rate predictable.
-		// One alternative would be to switch to a no-reservation mode (Allow() method) whenever we are in such a low rate regime.
-		if rtl < 1 {
-			maxDelay = 500 * time.Millisecond
-		} else {
-			maxDelay = time.Second / (time.Duration(rtl) * 2)
+
+		maxDelay, err = time.ParseDuration(config.MaxDelay)
+		if maxDelay == 0 || err != nil {
+			// maxDelay does not scale well for rates below 1,
+			// so we just cap it to the corresponding value, i.e. 0.5s, in order to keep the effective rate predictable.
+			// One alternative would be to switch to a no-reservation mode (Allow() method) whenever we are in such a low rate regime.
+			if rtl < 1 {
+				maxDelay = 500 * time.Millisecond
+			} else {
+				maxDelay = time.Second / (time.Duration(rtl) * 2)
+			}
 		}
-
-		maxDelay = 5 * time.Second
-		log.Print(fmt.Errorf("max delay: %f", maxDelay))
 	}
 
-	// Make the ttl inversely proportional to how often a rate limiter is supposed to see any activity (when maxed out),
-	// for low rate limiters.
-	// Otherwise just make it a second for all the high rate limiters.
-	// Add an extra second in both cases for continuity between the two cases.
-	ttl := 1
-	if rtl >= 1 {
-		ttl++
-	} else if rtl > 0 {
-		ttl += int(1 / rtl)
-	}
 
 	return &requestShaper{
 		name:          name,
 		rate:          rate.Limit(rtl),
 		burst:         burst,
 		maxDelay:      maxDelay,
+		exceedWait:    exceedWait,
 		next:          next,
 		buckets:       buckets,
-		ttl:           ttl,
+		ttl:           int(ttl),
 	}, nil
 }
 
 
 func (rs *requestShaper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// if rate is 0, perform no shaping
+	if rs.rate == 0 {
+		rs.next.ServeHTTP(w, r)
+		return
+	}
 
 	source, _, err := net.SplitHostPort(r.RemoteAddr)
 
 	if err != nil {
 		http.Error(w, "could not extract source of request", http.StatusInternalServerError)
+		log.Printf("Error extracting IP: %s", err)
 		return
 	}
 
@@ -142,15 +161,11 @@ func (rs *requestShaper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// because we want to update the expiryTime everytime we get the source,
 	// as the expiryTime is supposed to reflect the activity (or lack thereof) on that source.
 	if err := rs.buckets.Set(source, bucket, rs.ttl); err != nil {
-
+		log.Printf("Error updating bucket: %s", err)
 		http.Error(w, "could not insert/update bucket", http.StatusInternalServerError)
 		return
 	}
 
-	// time/rate is bugged, since a rate.Limiter with a 0 Limit not only allows a Reservation to take place,
-	// but also gives a 0 delay below (because of a division by zero, followed by a multiplication that flips into the negatives),
-	// regardless of the current load.
-	// However, for now we take advantage of this behavior to provide the no-limit ratelimiter when config.Average is 0.
 	res := bucket.Reserve()
 	if !res.OK() {
 		http.Error(w, "No bursty traffic allowed", http.StatusTooManyRequests)
@@ -159,7 +174,9 @@ func (rs *requestShaper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	delay := res.Delay()
 	if delay > rs.maxDelay {
+		// sleep to mitigate attacks
 		res.Cancel()
+		time.Sleep(rs.exceedWait)
 		rs.serveDelayError(w, delay)
 		return
 	}
@@ -172,6 +189,6 @@ func (rs *requestShaper) serveDelayError(w http.ResponseWriter, delay time.Durat
 	w.WriteHeader(http.StatusTooManyRequests)
 
 	if _, err := w.Write([]byte(http.StatusText(http.StatusTooManyRequests))); err != nil {
-
+		log.Printf("Error serving delay error: %s", err)
 	}
 }
